c1493110e7e0274d896f6abbe2c9208e008213e0
burcudumanl <153668213+burcudumanl@users.noreply.github.com>
2025-03-27 17:14:22 +0300

    LAB6.py

--- LAB6.py
+++ LAB6.py
@@ -1,22 +1,29 @@
 from math import pi
 
+
 def factorial(x):
     if x == 0 or x == 1:
         return 1
     return x * factorial(x - 1)
 
-def degree_to_radiana(degree):
+
+def degree_to_radian(degree):
     return degree * (pi / 180)
 
-def sine_x(x_degrees, terms=10):
+
+single_step=lambda n,x:((-1)**n)*(x**(2*n+1))/factorial(2*n+1)
+
+
+def sine_x(x_degrees, n):
     x = degree_to_radian(x_degrees)
     result = 0
-    for n in range(terms):
-        term = ((-1) ** n) * (x ** (2 * n + 1)) / factorial(2 * n + 1)
-        result += term
+    for i in range(n):
+        result += single_step(i, x)
     return result
 
+
 harmonic_sum = 0
+
 
 def harmonic(n):
     global harmonic_sum
@@ -26,12 +33,15 @@ def harmonic(n):
     harmonic_sum = harmonic(n - 1) + (1 / n)
     return harmonic_sum
 
+
 if __name__ == "__main__":
     # 1
-    print("Factorial (5!):", factorial(5)) 
+    print("Factorial (5!):", factorial(5))
+    x_degrees = float(input("Please enter the degree of x: "))
+    n = int(input("Please enter the numbers of terms n: "))
 
-    #2
-    print("sin(30):", my_sin(30)) 
-    
+    # 2
+    print("sin(30):", sine_x(x_degrees,n))
+
     # 3
     print("H_5:", harmonic(5))
